7625ee71b49a3f0b34ee486a585a2171dd58ee6d
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-11-14 00:33:38 +0530

    Created sidebar and chat components as well as did the cleanup and project setup. Created firebase.js which has all the config in JSON.

---
+++ whatsapp-mern/src/components/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat"
+
+function App() {
+  return (
+    <div className="app">
+        <Sidebar/>
+        <Chat/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+const firebaseConfig = {
+  apiKey: "AIzaSyAz8gSh06biWiweIr6GR4HjK59PU5gxafM",
+  authDomain: "whatsapp-mern-8f079.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-8f079.firebaseio.com",
+  projectId: "whatsapp-mern-8f079",
+  storageBucket: "whatsapp-mern-8f079.appspot.com",
+  messagingSenderId: "270081549277",
+  appId: "1:270081549277:web:d05d1ee02f83f2404e0c34",
+};
