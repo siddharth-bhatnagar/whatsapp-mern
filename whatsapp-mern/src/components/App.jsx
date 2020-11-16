0055b61fac80ffef1632888af29ccf0b92830cc2
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-11-16 21:16:51 +0530

    added pusher to the app

--- whatsapp-mern/src/components/App.jsx
+++ whatsapp-mern/src/components/App.jsx
@@ -1,13 +1,43 @@
 import "./css/App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
+import { useEffect, useState } from "react";
+import axios from "C:\\Users\\neoyo\\Desktop\\react-projects\\whatsapp-clone\\whatsapp-mern\\src\\axios.js";
 
 function App() {
+
+  const [messages, setMessages] =  useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync').then(response => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher('a92c6d95392ac1fe7af9', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages = {messages} />
       </div>
     </div>
   );
--- whatsapp-mern/src/components/Chat.jsx
+++ whatsapp-mern/src/components/Chat.jsx
@@ -5,7 +5,7 @@ import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from '@material-ui/icons/Mic';
 
-function Chat() {
+function Chat(prop) {
   return (
     <div className="chat">
       <div className="chat__header">
